81d91411853e344a8c641b27dce979711f6eb744
Saqib Mohammad <saqib117@gmail.com>
2012-02-09 15:07:52 -0800

    First implemented test code... hello Android

--- android/src/com/because/wimote/WiMoteActivity.java
+++ android/src/com/because/wimote/WiMoteActivity.java
@@ -2,12 +2,18 @@ package com.because.wimote;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class WiMoteActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+       // setContentView(R.layout.main);
+        
+        
+        TextView tv = new TextView(this);
+        tv.setText("I am the WiMote Rawr!");
+        setContentView(tv);
     }
 }
