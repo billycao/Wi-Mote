addaced41b0ef54f8d60dfa9e81faa92a5839536
Billy Cao <billy.cao@ucla.edu>
2012-02-08 17:45:12 -0800

    Initial Commit
    Added android project skeleton

---
+++ android/src/com/awesome/wimote/WiMoteActivity.java
@@ -0,0 +1,13 @@
+package com.awesome.wimote;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WiMoteActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
