a0ed059553204c676a8a4bedc139495df13d32ac
Billy Cao <billy.cao@ucla.edu>
2012-02-25 17:57:01 -0800

    new preferences system

--- android/src/com/because/wimote/Preference_Activity.java
+++ android/src/com/because/wimote/Preference_Activity.java
@@ -8,6 +8,8 @@ public class Preference_Activity extends PreferenceActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
-		
+		addPreferencesFromResource(R.xml.preferences_accel);
+		addPreferencesFromResource(R.xml.preferences_keyboard);
+		addPreferencesFromResource(R.xml.preferences_trackpad);
 	}
 }
